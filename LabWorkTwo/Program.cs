5e8f8b3ff298e16d715b1f4d60c220460d0f08dd
ssplant <76212491+ssplant@users.noreply.github.com>
2021-01-01 22:33:02 +0900

    Add project files.

---
+++ LabWorkTwo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace LabWorkTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputKey;
+            Console.WriteLine("Введите любой символ:");
+            for (;;)
+            {
+                if (int.TryParse(Console.ReadLine(), out inputKey))
+                {
+                    Console.WriteLine(inputKey + " является цифрой");
+                }
+                else
+                {
+                    Console.WriteLine("Данный символ не является цифрой");
+                }
+            }
+        }
+    }
+}
